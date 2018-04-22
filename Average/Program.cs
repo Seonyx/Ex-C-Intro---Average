5e3f559aba68cc8b85961f1c9238a0e9304b58eb
Seonyx <info@seonyx.com>
2018-04-22 15:14:39 +0200

    Add project files.

---
+++ Average/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            Console.WriteLine("Enter number 1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            number1 = number1 + Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            number1 = number1 + Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 4");
+            number1 = number1 + Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The average of the four numbers is: {0}", number1 / 4);
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Average
+
+Write a C# program to calculate and display the average of four numbers entered by the user
